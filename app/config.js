5e099fa8c18f88586a5447fa8554f0d27c1c9962
Grzegorz Trukan <gtrukan@membr.com>
2016-11-17 13:00:26 +0100

    small config fix

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 export default {
   version: '1.0',
-  restAPIURL: 'http://scrum.localhost/app_dev.php',
+  restAPIURL: 'http://pgs-poker.com/app_dev.php',
   env: process.env.NODE_ENV || 'development',
   socketURL: 'http://localhost:8090',
 };
