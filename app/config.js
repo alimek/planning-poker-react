c0ad0cb29e82be982f62fc064c1375020c12eacd
Grzegorz Mandziak <alimek@alimek.pl>
2017-03-07 16:29:37 +0100

    update dep

--- app/config.js
+++ app/config.js
@@ -2,5 +2,5 @@ export default {
   version: '1.0',
   restAPIURL: 'http://symfony.dev:8088/app_dev.php',
   env: process.env.NODE_ENV || 'development',
-  socketURL: 'http://localhost:8080',
+  socketURL: 'http://localhost:8090',
 };
